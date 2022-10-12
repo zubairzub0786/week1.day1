5015ea719b9f1b95016ef6afd22aaeaf6806f277
zubairzub0786 <115525529+zubairzub0786@users.noreply.github.com>
2022-10-12 11:32:12 +0530

    Add files via upload
    week1.day1

---
+++ day1/Mobilephone.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class Mobilephone {
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+		String mobileBrandName= "vivo";
+		System.out.println(mobileBrandName);
+		char mobileLogo=   'O';
+		System.out.println(mobileLogo);
+		short noOfMobilePiece= 10;
+		System.out.println(noOfMobilePiece);
+		int mobileNumber= 2021;
+		System.out.println(mobileNumber);
+		long mobileImeiNumber= 94568989909L;
+		System.out.println(mobileImeiNumber);
+		float mobilePrice=34456.987F;
+		System.out.println(mobilePrice);
+		boolean isDamaged= false;
+		System.out.println(isDamaged);
+
+		}
+
+}
